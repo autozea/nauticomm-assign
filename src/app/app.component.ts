141d47d1043118237f69a735dc924529688c1e7d
Chutikan Pankam <p_chutikan@kkumail.com>
2022-06-27 19:06:54 +0700

    location direction

---
+++ src/app/app.component.ts
@@ -0,0 +1,103 @@
+import { MapsAPILoader, AgmCoreModule } from '@agm/core';
+import {
+  Component,
+  ElementRef,
+  NgZone,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'go-around';
+  zoom: number = 8;
+  address: string = '';
+  changeAddress: string = '';
+  latitude: number = 13.746345642630601;
+  longitude: number = 100.56279136117175;
+  private geoCoder: any;
+  public origin: any;
+  public destination: any;
+
+  @ViewChild('search')
+  public searchRef!: ElementRef;
+
+  constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.getDirection();
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder();
+      let autocomplete = new google.maps.places.Autocomplete(
+        this.searchRef.nativeElement
+      );
+      autocomplete.addListener('place_changed', () => {
+        this.ngZone.run(() => {
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 10;
+          let changeAddress = place.formatted_address;
+          console.log('Change address: ', changeAddress);
+        });
+        this.getDestination();
+      });
+    });
+  }
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 12;
+        this.getAddress(this.latitude, this.longitude);
+        console.log(
+          'Your location = \n',
+          `lat: ${position.coords.latitude}, lng: ${position.coords.longitude}`
+        );
+        this.getDirection();
+      });
+    }
+  }
+
+  getAddress(latitude: number, longitude: number) {
+    this.geoCoder.geocode(
+      { location: { lat: latitude, lng: longitude } },
+      (results: any, status: any) => {
+        if (status === 'OK') {
+          if (results[0]) {
+            this.zoom = 12;
+            this.address = results[0].formatted_address;
+          } else {
+            window.alert('Not found');
+          }
+        }
+        console.log('Youe location is: ', this.address);
+      }
+    );
+  }
+
+  getDirection() {
+    this.origin = { lat: this.latitude, lng: this.longitude };
+    this.destination = { lat: 13.746345642630601, lng: 100.56279136117175 };
+    console.log('ori: ', this.origin);
+    console.log('des: ', this.destination);
+  }
+
+  getDestination() {
+    this.destination = { lat: this.latitude, lng: this.longitude };
+    console.log('destination check: ', this.destination);
+  }
+
+  mapClicked($event: google.maps.MouseEvent) {
+    console.log('Now we go: ', this.latitude, this.longitude);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { AgmCoreModule } from '@agm/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { AgmDirectionModule } from 'agm-direction';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAEcblsEgtaqEk_TV4c2sG-uO3i3KHRXzM',
+      libraries: ['places', 'geometry'],
+    }),
+    AgmDirectionModule,
+    MatIconModule,
+    MatInputModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
